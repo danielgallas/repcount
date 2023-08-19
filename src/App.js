9de4587e398464ca1d12d88853fbeac32832c0b8
Daniel Gallas <danielgallas2@gmail.com>
2023-08-19 15:34:09 +0100

    includes rep add and subtract

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Points from "./Components/Points";
-import Button from "./Components/Button";
+import ChooseExercise from "./Components/ChooseExercise";
+import SetExercise from "./Components/SetExercise";
 import React, { useState } from "react";
 
-export const Daniel = React.createContext();
+export const ToggleContext = React.createContext();
 
 function App() {
   const [toggle, setToggle] = useState(true);
+  const [workout, setWorkout] = useState("");
 
   return (
     <div className="grid-container">
-      <Daniel.Provider value={{ toggle, setToggle }}>
+      <ToggleContext.Provider
+        value={{ toggle, setToggle, workout, setWorkout }}
+      >
         <Header />
         <Points />
         {toggle ? (
           <>
-            <Button exercise={"Burpee"} />
-            <Button exercise={"Plank"} />
-            <Button exercise={"Push-up"} />
+            <ChooseExercise exercise={"Burpee"} />
+            <ChooseExercise exercise={"Plank"} />
+            <ChooseExercise exercise={"Push-up"} />
           </>
         ) : (
-          <Button exercise={"Plank"} />
+          <SetExercise exercise={workout} />
         )}
-      </Daniel.Provider>
+      </ToggleContext.Provider>
     </div>
   );
 }
